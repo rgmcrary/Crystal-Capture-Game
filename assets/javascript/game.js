6f66a77dd32a546bf38489c585e7f01763c4509c
Rich McRary <rgmcrary@gmail.com>
2017-07-14 20:39:19 -0400

    Finished Project
    Completed homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // Variables ####
 
 var randNum;
@@ -6,79 +5,74 @@ var currentTally;
 var wins = 0;
 var losses = 0;
 
-
-
-    // Gameplay #####
-
-
+// Gameplay #####
 
 var game = {
 
 
-    // Random number generator #####
 
-start: function () {
-        currentTally = 0;
-        $("#score").text(currentTally);
-        //$("#winLosses").text("");
+  // Random number generator #####
 
-        randNum = this.getRandomNumber(19, 120);
-        $("#diamond").val(this.getRandomNumber(1, 12));
-        $("#sapphire").val(this.getRandomNumber(1, 12));
-        $("#ruby").val(this.getRandomNumber(1, 12));
-        $("#emerald").val(this.getRandomNumber(1, 12));
-        $("#targetNumber").text(randNum);
-},
+  start: function() {
+    currentTally = 0;
+    $("#score").text(currentTally);
+    //$("#winLosses").text("");
 
-getRandomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-},
+    randNum = this.getRandomNumber(19, 120);
+    $("#diamond").val(this.getRandomNumber(1, 12));
+    $("#sapphire").val(this.getRandomNumber(1, 12));
+    $("#ruby").val(this.getRandomNumber(1, 12));
+    $("#emerald").val(this.getRandomNumber(1, 12));
+    $("#targetNumber").text(randNum);
+  },
 
-takeTurn: function (event) {
+  getRandomNumber: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
 
-    // Game Loop ################
-            // Score Tally #####
+  takeTurn: function(event) {
+    // Game Loop ################`
+    // Score Tally #####
 
-        var guess = parseInt(event.target.value);
-        currentTally = guess + currentTally;
-        $("#score").text(currentTally);
+    var guess = parseInt(event.target.value);
+    currentTally = guess + currentTally;
+    $("#score").text(currentTally);
 
-            //  Playing Conditions
-        if (currentTally === randNum) {
-            wins++;
-            game.updateDisplay("YOU DID IT!!!", "win");
-            game.start();
-        }
-        else if (currentTally > randNum) {
-            losses++;
-            game.updateDisplay("TOUGH LUCK...BETTER LUCK NEXT TIME", "loss");
-            game.start();
-        }
-},
-             // Win/Loss Scoring #####
-
-        updateDisplay: function (resultText, updateType) {
-        $("#winLosses").text(resultText);
-        if (updateType === "win") {
-            $("#wins").text(wins);
-        } 
-        else {
-            $("#losses").text(losses);
-        }
+    //  Playing Conditions
+    if (currentTally === randNum) {
+      wins++;
+      game.updateDisplay("YOU DID IT!!!", "win");
+      game.start();
+    } else if (currentTally > randNum) {
+      losses++;
+      game.updateDisplay("TOUGH LUCK...BETTER LUCK NEXT TIME", "loss");
+      game.start();
     }
+  },
+  // Win/Loss Scoring #####
+
+  updateDisplay: function(resultText, updateType) {
+    alert(resultText);
+    if (updateType === "win") {
+      $("#wins").text(wins);
+    } else {
+      $("#losses").text(losses);
+    }
+  }
 };
 
-
-$(document).ready(function () {
-        game.start();
-        $("#diamond").on("click", game.takeTurn);
-        $("#sapphire").on("click", game.takeTurn);
-        $("#ruby").on("click", game.takeTurn);
-        $("#emerald").on("click", game.takeTurn);
+$(document).ready(function() {
+  game.start();
+  $("#diamond").on("click", game.takeTurn);
+  $("#sapphire").on("click", game.takeTurn);
+  $("#ruby").on("click", game.takeTurn);
+  $("#emerald").on("click", game.takeTurn);
 });
-
 
 // Buttons #####
 
+// make the updateDisplay an alert
+
+// can use any layout as long as it works as required
